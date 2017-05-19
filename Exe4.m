2ec141182d3102410e661a3fb07f85dacbe63e33
raafaadg <raafaadg@gmail.com>
2017-05-19 09:19:55 -0300

    Fix Erro
    Concertado bug referente a  apresentação das iterações

--- Exe4.m
+++ Exe4.m
@@ -27,8 +27,7 @@ switch metodo  %Seleciona o método
         for k=1:size(LAMB,2)
             Names(k)={sprintf('Iteração nº%d',k)};
         end
-        T = table(A(1,1:end-1)',B(1,1:end-1)',LAMB',double(f(LAMB')),...
-            ERRO(1,1:end-1)','VariableName',Vars,'RowNames',Names);
+        T = table(A',B',LAMB',double(f(LAMB')),ERRO','VariableName',Vars,'RowNames',Names);
     case 1
         [A,B,LAMB,U,ERRO]=sec_aurea(f,a,b);   %Método da Seção Aurea %f=função, a=ponto inical, b=ponto inicial
         Vars={'a' 'b' 'Lambda' 'Mi_u' 'f_lamb' 'f_mi' 'Erro'};
--- bissecao.m
+++ bissecao.m
@@ -3,8 +3,9 @@ it=1;           %Variável para controlar as iterações
 df=diff(f);  %Derivada da função
 erro(it)=abs(double(f(b))-double(f(a)));    %Calcula erro inicial
 % erro(it)=abs(b(it)-a(it));
+lamb(it)=(a(it)+b(it))/2;
 while(erro(it)>1e-3)    %Execute enquanto e erro for menor que 10^-3
-   lamb(it)=(a(it)+b(it))/2; %Calcula alfa para os pontos a e b ionformados
+   
    if(double(df(lamb(it)))<0)       %Caso alfa sejá negativo
        a(it+1)=lamb(it);            % a seguinte recebe alfa atual
        b(it+1)=b(it);               % b seguinte recebe b atual
@@ -21,6 +22,7 @@ while(erro(it)>1e-3)    %Execute enquanto e erro for menor que 10^-3
    it=it+1; %Incrementa a variavél de controle da iteração
    erro(it)=abs(double(f(b(it)))-double(f(a(it))));%Calcula erro atual
 %    erro(it)=abs(b(it)-a(it));
+   lamb(it)=(a(it)+b(it))/2; %Calcula alfa para os pontos a e b ionformados
 end
 disp('------------------------------------------------------------------');
 disp('FIM!');
